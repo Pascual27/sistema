680de7eb25217a8873e4130a446f73208a60c31c
Pascual27 <115171634+Pascual27@users.noreply.github.com>
2022-12-12 12:44:20 -0500

    wq

--- include/menu.php
+++ include/menu.php
@@ -16,8 +16,8 @@
                 <img src="imagen/img.jpg" alt="..." class="img-circle profile_img">
               </div>
               <div class="profile_info">
-                <span>Welcome,</span>
-                <h2>John Doe</h2>
+                <span>Bienvenido,</span>
+                <h2>Pascual Ñaupa</h2>
               </div>
             </div>
             <!-- /menu profile quick info -->
