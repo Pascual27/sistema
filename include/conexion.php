b1243b3e025ab427d49066494f1e058405e36ebb
Pascual27 <115171634+Pascual27@users.noreply.github.com>
2022-12-12 10:20:10 -0500

    df

--- include/conexion.php
+++ include/conexion.php
@@ -1,7 +1,6 @@
 <?php
 // conexion utiliza servidor-usuario-password-base de datos
-$conexion = mysqli_connect("localhost", "	
-ceicom_pascual", "71068043.ñ", "ceicom_pascual");
+$conexion = mysqli_connect("localhost", "ceicom_pascual", "71068043.ñ", "ceicom_pascual");
 //$conexion = mysqli_connect("localhost", "root", "root", "sistema");
 if ($conexion) {
 
