77e60ea71f7779b3246f9c384990d5f40a13c0b4
Pascual27 <115171634+Pascual27@users.noreply.github.com>
2022-12-12 16:59:57 -0500

    dwa

--- operaciones/registrar_periodo_acad.php
+++ operaciones/registrar_periodo_acad.php
@@ -1,35 +1,27 @@
-<?php
+<?php 
 include "../include/conexion.php";
-include "../include/busquedas.php";
-$nombr = $_POST['nombre'];
-$fecha_ini = $_POST['fecha_inicio'];
-$fecha_fin = $_POST['fecha_fin'];
-$director = $_POST['director'];
-$fecha_actas = $_POST['fecha_actas'];
 
-$b_periodo_academico = buscarPeriodoAcademico($conexion, $cod);
-$c_r_b_periodo_academico = mysqli_num_rows($b_periodo_academico);
-if ($c_r_b_periodo_academico == 0) {//validamos que no haya registros en la base de datos
-	$insertar = "INSERT INTO periodo_academico (nombre, fecha_inicio, fecha_fin, director, fecha_actas) VALUES ('$nombr','$fecha_ini','$fecha_fin', '$director','$fecha_actas')";
-	$ejecutar_insetar = mysqli_query($conexion, $insertar);
-    $b_id_periodo_academico = buscarPeriodoAcademico($conexion, $cod);
-	$res_b_periodo_academico = mysqli_fetch_array($b_id_periodo_academico);
-    $id_periodo_academico = $res_b_periodo_academico['id'];
-    
-	$ejec_insert_usu = mysqli_query($conexion, $insertar_usu);
-	if ($ejec_insert_usu) {
-		echo "<script>
+// traemos los datos  de neustro form para registrar el periodo academico
+$p_academico  = $_POST['nombre'];
+$fecha_inicio  = $_POST['fecha_inicio'];
+$fecha_fin  = $_POST['fecha_fin'];
+$director  = $_POST['director'];
+$fecha_actas  = $_POST['fecha_actas'];
+
+$sql = "INSERT INTO periodo_academico (nombre, fecha_inicio, fecha_fin, id_director, fecha_actas ) VALUES ('$p_academico', '$fecha_inicio', '$fecha_fin', '$director', '$fecha_actas')";
+
+$ejec =mysqli_query($conexion, $sql);
+
+if ($sql) {
+    echo "<script>
                 alert('Registro Exitoso');
                 window.location= '../periodo_academico.php'
     			</script>";
-	}else{
-		echo "<script>
-			alert('Error al registrar periodo academico');
-			window.history.back();
-			</script>
-			";
-	}
+}else{
+    echo "<script>
+        alert('Error al registrar usuario');
+        window.history.back();
+        </script>
+        ";
 }
-
-
-?> 
+?>
