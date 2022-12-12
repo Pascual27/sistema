adb5d415a247e38a5f94553a1eccc28298a18759
Pascual27 <115171634+Pascual27@users.noreply.github.com>
2022-12-12 12:14:34 -0500

    cdsf

--- include/menu.php
+++ include/menu.php
@@ -12,7 +12,7 @@
             <!-- menu profile quick info -->
             <div class="profile clearfix">
               <div class="profile_pic">
-                <img src="" alt="..." class="">
+                <img src="" alt="include/img.jpg" class="">
               </div>
               <div class="profile_info">
                 <span>Bienvenido,</span>
@@ -98,20 +98,10 @@
               <ul class="nav navbar-nav navbar-right">
                 <li class="">
                   <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                    <img src="Gentella/production/images/img.jpg" alt=""><?php echo $res_b_u_s['Pascual Ñaupa Cruz']; ?>
+                    <img src="include/img.jpg" alt=""><?php echo $res_b_u_s['Pascual Ñaupa Cruz']; ?>
                     <span class=" fa fa-angle-down"></span>
                   </a>
-                  <ul class="dropdown-menu dropdown-usermenu pull-right">
-                    <li><a href="javascript:;"> Perfil</a></li>
-                    <li>
-                      <a href="javascript:;">
-                        <span class="badge bg-red pull-right">50%</span>
-                        <span>Settings</span>
-                      </a>
-                    </li>
-                    <li><a href="javascript:;">Help</a></li>
-                    <li><a href="operaciones/cerrar_sesion.php"><i class="fa fa-sign-out pull-right"></i> Log Out</a></li>
-                  </ul>
+                  
                 </li>
 
                 <li role="presentation" class="dropdown">
@@ -122,14 +112,12 @@
                   <ul id="menu1" class="dropdown-menu list-unstyled msg_list" role="menu">
                     <li>
                       <a>
-                        <span class="image"><img src="images/img.jpg" alt="Profile Image" /></span>
+                        <span class="image"><img src="include/img.jpg" alt="Profile Image" /></span>
                         <span>
                           <span>Pascual Ñaupa Cruz</span>
                           <span class="time">3 mins ago</span>
                         </span>
-                        <span class="message">
-                          Film festivals used to be do-or-die moments for movie makers. They were where...
-                        </span>
+                        
                       </a>
                     </li>
                     <li>
@@ -139,20 +127,17 @@
                           <span>Pascual Ñaupa</span>
                           <span class="time">3 mins ago</span>
                         </span>
-                        <span class="message">
-                          Film festivals used to be do-or-die moments for movie makers. They were where...
-                        </span>
+                        
                       </a>
                     </li>
                     <li>
                       <a>
-                        <span class="image"><img src="Gentella/production/images/img.jpg" alt="Profile Image" /></span>
+                        <span class="image"><img src="include/img.jpg" alt="Profile Image" /></span>
                         <span>
                           <span>Pascual Ñaupa</span>
                           <span class="time">3 mins ago</span>
                         </span>
-                        <span class="message">
-                          Film festivals used to be do-or-die moments for movie makers. They were where...
+                        
                         </span>
                       </a>
                     </li>
@@ -163,9 +148,7 @@
                           <span>John Smith</span>
                           <span class="time">3 mins ago</span>
                         </span>
-                        <span class="message">
-                          Film festivals used to be do-or-die moments for movie makers. They were where...
-                        </span>
+                        
                       </a>
                     </li>
                     <li>
