ad7eae39438d1cf3a5aff509e4a62ed68cd9c8a1
Pascual27 <115171634+Pascual27@users.noreply.github.com>
2022-12-12 15:59:46 -0500

    df

--- login.php
+++ login.php
@@ -26,7 +26,9 @@
     <div>
       <a class="hiddenanchor" id="signup"></a>
       <a class="hiddenanchor" id="signin"></a>
-
+      kcenter>
+<img-src="./imgen/12.png" alt="logo instituto" width="150px"/›
+</center>
       <div class="login_wrapper">
         <div class="animate form login_form">
           <section class="login_content">
